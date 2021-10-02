904bdd54b8ff05b84eec59fd95d6d5bb6c175852
Sunday <dev.nattapon.s@gmail.com>
2021-10-02 08:01:33 +0700

    fix: vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,20 +8,21 @@ import { Pet } from './pets/pet.entity';
 import { PetsController } from './pets/pets.controller';
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb+srv://root:hardc0deX@finxcluster.epdly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-      database: 'finxCluster',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      ssl: true,
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      j: true,
-    }),
-    TypeOrmModule.forFeature([Pet]),
+    // ConfigModule.forRoot(),
+    // TypeOrmModule.forRoot({
+    //   type: 'mongodb',
+    //   url: 'mongodb+srv://root:hardc0deX@finxcluster.epdly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    //   database: 'finxCluster',
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   ssl: true,
+    //   useUnifiedTopology: true,
+    //   useNewUrlParser: true,
+    //   j: true,
+    // }),
+    // TypeOrmModule.forFeature([Pet]),
   ],
-  controllers: [AppController, PetsController],
+  // controllers: [AppController, PetsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
