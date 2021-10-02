bb9bdb79a877262e7ecfc0d3d2b555d82c3019e2
Sunday <dev.nattapon.s@gmail.com>
2021-10-02 07:42:34 +0700

    :tada: config mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { Pet } from './pets/pet.entity';
+import { PetsController } from './pets/pets.controller';
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://root:hardc0deX@finxcluster.epdly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      database: 'finxCluster',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+    TypeOrmModule.forFeature([Pet]),
+  ],
+  controllers: [AppController, PetsController],
   providers: [AppService],
 })
 export class AppModule {}
